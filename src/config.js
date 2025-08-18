583a2a5f73729bd63c27f10d0e7c43851cc5fefe
BODDAPU CHANDRA SEKHAR <147380240+boddapuchandrasekhar@users.noreply.github.com>
2025-08-18 20:22:28 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,2 @@
-// src/config.js
-const API_BASE_URL = "https://1e085dd40dd8.ngrok-free.app"; // change only here
-
+const API_BASE_URL = "https://backend-production-723b.up.railway.app"; 
 export default API_BASE_URL;
